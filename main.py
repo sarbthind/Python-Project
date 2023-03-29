fa8de4ae3c050c08b2596494659472bf794e93c4
sarbthind <117028155+sarbthind@users.noreply.github.com>
2023-03-30 01:10:46 +0530

    adding startapp function and searchword function.

--- main.py
+++ main.py
@@ -57,3 +57,43 @@ def operate(num):
         num = input()
         operate(num)
 
+def searchWord():
+    myDictionary = open("./dictionary.txt", "r")
+    allWords = myDictionary.readlines()
+    wordToSearch= input(f"{bcolors.BOLD}Please enter the Word You wanna Search:")
+    flag = True
+    for x in allWords:
+        if f"{wordToSearch}:" in x:
+            print("the Meaning of the word you searched is: \n")
+            print(f"{bcolors.UNDERLINE}{x}{bcolors.ENDC}")
+            flag = False
+            myDictionary.close()
+
+    if flag:
+        myDictionary.close()
+        print(f"{bcolors.FAIL}you searched word is not present in dictionary. \n if you want correction press: c \n if you wanna add new word press a\n{bcolors.ENDC}")
+        num = input(": ")
+        operate(num)
+    else:
+        print(f"{bcolors.OKCYAN}Wanna search more words press y otherwise n.{bcolors.ENDC}")
+        onion = input()
+        if onion =="y":
+            searchWord()
+        else:
+            print(f"{bcolors.OKGREEN}thanks for using our App...{bcolors.ENDC}")
+
+def startApp():
+    print(f"{bcolors.HEADER}press 1 to search into the dictionary. \npress 2 to add word into the dictionary.\nPress 3 to delete word in dictionary.{bcolors.ENDC}")
+    print(": ",end="")
+    userInput = input()
+    print(userInput)
+    if userInput =="1":
+        searchWord()
+    elif userInput =="2":
+        addWord()
+    elif userInput =="3":
+        delete()
+    else:
+        print(f"{bcolors.FAIL}You entered incorrect input. Please enter again: \n:{bcolors.ENDC} ")
+        startApp()
+startApp()
